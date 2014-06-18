8cdf28c9ccee45baea6dbd2776bb454a7c9bb22f
Hernan <Hernan@Hernan-PC>
2014-06-17 23:24:46 -0300

    Prueba

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here fsafaukh
+        // put your code here fsafaukhkgy
         ?>
     </body>
 </html>
